2c5fd53eca69463b7c5f2b5dcf0f3f52b490fc10
Elijah Prince <elijahprince@Elijahs-Mac-mini.local>
2018-12-05 23:09:35 -0800

    Added styles to app and used props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    
     return (
-        <Text>
-            Albums
-        </Text>
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
     );
 };
 
-export default Header
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 20,
+        marginTop: 20,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2, },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        postiton: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+
+export default Header;
